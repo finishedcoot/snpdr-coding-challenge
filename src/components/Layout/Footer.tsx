fb5377e27ca14731468057eddd508e34326a4aa1
Ardeshir <ardeshiir@outlook.com>
2024-04-15 08:48:12 +0000

    feat: Add Footer component

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className={'mt-4 w-full flex justify-center items-center py-3 border-t text-xs text-slate-500'}>
+            copyright by ©CODE CHALLENGE
+        </div>
+    );
+};
+
+export default Footer;
